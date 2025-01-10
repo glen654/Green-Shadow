863bf131f8817f6e36dcc704a3765b947dd3bb41
glen654 <glenalloy@gmail.com>
2025-01-10 11:27:09 +0530

    implemented route paths to the sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
+import { createBrowserRouter, RouterProvider } from 'react-router'
 import './App.css'
-import Navigation from './components/Navigation'
+import { RootLayout } from './components/RootLayout'
+import { Dashboard } from './pages/Dashboard'
+import { Field } from './pages/Field'
+import { Crop } from './pages/Crop'
+import { Staff } from './pages/Staff'
+import { Equipment } from './pages/Equipment'
+import { Log } from './pages/Log'
+import { Vehicle } from './pages/Vehicle'
 
 function App() {
-  
+  const routes = createBrowserRouter([
+    {
+      path:"",
+      element:<RootLayout/>,
+      children:[
+        {path: '', element: <Dashboard/>},
+        {path: '', element: <Field/>},
+        {path: '', element: <Crop/>},
+        {path: '', element: <Staff/>},
+        {path: '', element: <Equipment/>},
+        {path: '', element: <Log/>},
+        {path: '', element: <Vehicle/>}
+      ]
+    }
+  ])
 
   return (
     <>
-      <Navigation></Navigation>
+      <RouterProvider router={routes}></RouterProvider>
     </>
   )
 }
---
+++ src/components/RootLayout.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router";
+import { Sidebar } from "./Sidebar";
+
+
+export function RootLayout(){
+    return(
+        <>
+            <Sidebar></Sidebar>
+            <Outlet></Outlet>
+        </>
+    )
+}

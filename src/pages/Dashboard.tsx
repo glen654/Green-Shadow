b5b56d360413005880b72610cf78b0424d2027a9
glen654 <glenalloy@gmail.com>
2025-01-10 21:48:19 +0530

    implementing dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,10 @@
-export function Dashboard(){
-    return(
-        <>
-            <h1>Dashboard</h1>
-        </>
-    )
+import { Cards } from "../components/Cards";
+
+export function Dashboard() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold mt-6 ml-6 mb-2">Dashboard</h1>
+      <Cards />
+    </>
+  );
 }
